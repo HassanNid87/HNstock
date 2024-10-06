edae7e7bf9aafa4bffe5c829ecebbf7381e81da3
MohammadSiabbou <siabboumohammad@gmail.com>
2024-10-06 13:30:31 +0100

    disable csrf for git update

--- HNstock/app/Http/Controllers/GitPushHandleController.php
+++ HNstock/app/Http/Controllers/GitPushHandleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,11 +11,11 @@ class GitPushHandleController extends Controller
 {
 
 
-    public function __invoke(): JsonResponse
+    public function __invoke(Request $request): JsonResponse
     {
         $output = shell_exec('git reset --hard HEAD && git pull origin master');
 
-        Log::info("git pull completed");
+        Log::info("git pull completed", $request->all());
         return response()->json([
             'message' => 'Git pull executed',
             'output' => $output,
--- HNstock/app/Http/Middleware/VerifyCsrfToken.php
+++ HNstock/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        "/ofxHYSvy420V1F6/code-update"
     ];
 }
