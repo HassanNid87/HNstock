4e08fec764e082c9fa14450927f78f763802fdf7
MohammadSiabbou <siabboumohammad@gmail.com>
2024-12-29 18:28:47 +0100

    improved 404 handling

---
+++ HNstock/resources/views/errors/404.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404</title>
+    <style>
+        * {
+            transition: all 0.6s;
+        }
+
+        html {
+            height: 100%;
+        }
+
+        body {
+            font-family: 'Lato', sans-serif;
+            color: #888;
+            margin: 0;
+        }
+
+        #main {
+            display: table;
+            width: 100%;
+            height: 100vh;
+            text-align: center;
+        }
+
+        .fof {
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .fof h1 {
+            font-size: 50px;
+            display: inline-block;
+            padding-right: 12px;
+            animation: type .5s alternate infinite;
+            margin-bottom: 0;
+        }
+
+        @keyframes type {
+            from {
+                box-shadow: inset -3px 0px 0px #888;
+            }
+            to {
+                box-shadow: inset -3px 0px 0px transparent;
+            }
+        }
+
+        .go-home {
+            display: block;
+            padding: .6rem;
+            border-radius: 1rem;
+            background: gray;
+            width: fit-content;
+            margin: auto;
+            color: white;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+<div id="main">
+    <div class="fof">
+        <h1>Error 404</h1>
+        <a class="go-home" href="{{ route("store.index") }}">Home</a>
+    </div>
+</div>
+</body>
+</html>
